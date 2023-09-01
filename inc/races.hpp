323fcce896eab79919308200f17afee09da3f292
kacper <thespbsik@gmail.com>
2023-09-01 14:03:28 +0200

    adding human voice list

--- inc/races.hpp
+++ inc/races.hpp
@@ -2,14 +2,25 @@
 #define RACES_HPP
 #include <headers.hpp>
 
-class raceHuman: private cUnit
+class raceHuman : public cUnit
 {
-    int honor; //bonus bar for human race
+private:
+    int honor; // bonus bar for human race
+    std::vector<std::string> HumanVoiceList = {
+        "On duty!",
+        "Ready to combat",
+        "For Valor!",          // after attacking
+        "I will not forfeit!", // after getting damage
+    };
+
+public:
+    void VoiceHuman(int a);
 };
 
-class raceBarbarian: private cUnit
+class raceBarbarian : private cUnit
 {
-    int rage; //bonus bar for barbarians
+private:
+    int rage; // bonus bar for barbarians
 };
 
 #endif
